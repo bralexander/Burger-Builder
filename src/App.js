77e0a368078fab552c59f894ee1981e3cce3569e
Brad <bralexander@live.com>
2020-05-12 21:51:09 -0500

    created files/folders, added css for burger, displayed burger

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
-class App extends Component{
-  render(){
+class App extends Component {
+  render () {
     return (
       <div >
-        
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
 }
 
 export default App;
+
