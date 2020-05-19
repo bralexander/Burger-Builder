eb56ddebad6b9773b263bd6b25493086dc922e4f
Brad <bralexander@live.com>
2020-05-18 22:24:31 -0500

    added nav bar with stylized buttons and logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import Logo from '../../assets/images';
+import burgerLogo from '../../assets/images/burger-logo.png';
+
+import classes from './Logo.module.css';
 
 const logo = (props) => (
-    <div>
-        <img src/>
+    <div className={classes.Logo}>
+        <img src={burgerLogo} alt="MyBurger" />
     </div>
 );
 
